90db10a815bd190b5a4fefd31cde706bd6307772
Pyro <67813460+shloakvatsyayan@users.noreply.github.com>
2026-02-17 16:42:57 -0500

    fix: update commit message     # Please enter the commit message for your changes. Lines starting

--- main_mcstatus.py
+++ main_mcstatus.py
@@ -39,4 +39,4 @@ def get_bedrock_server_status():
         return jsonify({"online": False})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=2000)
+    app.run(host="127.0.0.1", port=8000)
